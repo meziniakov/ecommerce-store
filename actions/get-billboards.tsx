2655cf31137722190e8b231ec31c01459b0060b8
mezin <z2941@ya.ru>
2024-06-02 23:01:01 +0300

    feat: Billbords

--- actions/get-billboards.tsx
+++ actions/get-billboards.tsx
@@ -1,10 +1,10 @@
-import {Billboard as BillboardType} from '@/types/types'
+import { Billboard as BillboardType } from "@/types/types";
 
-const URL = `${process.env.NEXT_PUBLIC_API_URL}/billboards`
+const URL = `${process.env.NEXT_PUBLIC_API_URL}/billboards`;
 
-const getBillboards = async (id: string):Promise<BillboardType> => {
-    const res = await fetch(`${URL}/${id}`)
+const getBillboards = async (id: string): Promise<BillboardType> => {
+  const res = await fetch(`${URL}/${id}`);
 
-    return res.json()
-}
-export default getBillboards
+  return res.json();
+};
+export default getBillboards;
