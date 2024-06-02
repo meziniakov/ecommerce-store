2998755a914e4b635303e118399aa8ec74e4e5a7
mezin <z2941@ya.ru>
2024-06-02 23:00:36 +0300

    feat: ProductCard, ProductList, ProductPage, actions(getProduct, getProducts)

--- types/types.ts
+++ types/types.ts
@@ -1,12 +1,28 @@
 export interface Billboard {
-    id: string
-    label: string
-    imageUrl: string
+  id: string;
+  label: string;
+  imageUrl: string;
 }
 
 export interface Category {
-    id: string
-    name: string
-    image?: string
-    billboard?:Billboard
+  id: string;
+  name: string;
+  image?: string;
+  billboard?: Billboard;
+}
+
+export interface Product {
+  id: string;
+  category: Category;
+  name: string;
+  price: number | bigint;
+  isFeatured: boolean;
+  size?: string;
+  color?: string;
+  images: Image[];
+}
+
+export interface Image {
+  id: string;
+  url: string;
 }
